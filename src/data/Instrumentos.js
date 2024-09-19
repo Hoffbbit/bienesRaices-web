60cf8eac6f052b92ff4a2df597ed4b82102d65ee
Agustin Hoffmann <emiagu2018@outlook.es>
2024-09-19 17:25:31 -0300

    add: Contacto, Sobre nosotros ok

--- src/data/Instrumentos.js
+++ src/data/Instrumentos.js
@@ -1,4 +1,69 @@
+
+//SIMULACION BACKEND DE INSTRUMENTOS Y SUS PROPIEDADES//
+
 export const instrumentos = [
+    {
+      id: 1,
+      marca: "Fender",
+      tipo: "Electrica",
+      modelo: "Stratocaster",
+      cuerdas: 6,
+      pastillas: "Bobina Simple",
+      precio: 2000000,
+      operacion: "En venta",
+      origen: "Japon",
+      diapason: "Atornillado",
+      material: "Maple, Arce"
+    },
+    {
+      id: 2,
+      marca: "Romantica",
+      tipo: "Criolla",
+      modelo: "D-400",
+      cuerdas: 6,
+      precio: 120000,
+      operacion: "En venta",
+      origen: "Argentina",
+      diapason: "Encolado",
+      material: "Palo de rosa, caoba"
+    },
+    {
+      id: 3,
+      marca: "Leonard",
+      tipo: "Bajo",
+      modelo: "Stratocaster",
+      cuerdas: 4,
+      pastillas: "Bobina Doble",
+      precio: 300000,
+      operacion: "Encargue",
+      origen: "Belgica",
+      diapason: "Atornillado",
+      material: "Palo de rosa, roble"
+    },
+    {
+      id: 4,
+      marca: "Cort",
+      tipo: "Acustico",
+      modelo: "Quinto",
+      cuerdas: 4,
+      precio: 90000,
+      operacion: "En venta",
+      origen: "Argentina",
+      diapason: "Encolado",
+      material: "Caoba, palo de rosa"
+    },
+    {
+      id: 5,
+      marca: "Takamine",
+      tipo: "Acustica",
+      modelo: "Clasico",
+      cuerdas: 6,
+      precio: 600000,
+      operacion: "En venta",
+      origen: "Corea",
+      diapason: "Atornillado",
+      material: "Palisandro"
+    },
     {
       id: 1,
       marca: "Fender",
